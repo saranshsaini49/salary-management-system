71e2f90b1397f501405bafbb92a8fb6e7e8c0cf7
Saransh SAINI <saranshsss99@gmail.com>
2026-04-10 07:05:36 +0530

    fix: implement missing CRUD actions in EmployeesController including create endpoint

---
+++ backend/app/controllers/employees_controller.rb
@@ -0,0 +1,73 @@
+class EmployeesController < ApplicationController
+  before_action :set_employee, only: [:show, :update, :destroy]
+
+  # GET /employees
+  def index
+    employees = Employee.all
+
+    employees = employees.where(country: params[:country]) if params[:country].present?
+    employees = employees.where(job_title: params[:job_title]) if params[:job_title].present?
+
+    if params[:search].present?
+      employees = employees.where("full_name ILIKE ?", "%#{params[:search]}%")
+    end
+
+    page = params[:page].to_i > 0 ? params[:page].to_i : 1
+    per_page = params[:per_page].to_i > 0 ? params[:per_page].to_i : 20
+
+    employees = employees.offset((page - 1) * per_page).limit(per_page)
+
+    render json: employees
+  end
+
+  # GET /employees/:id
+  def show
+    render json: @employee
+  end
+
+  # POST /employees
+  def create
+    employee = Employee.new(employee_params)
+
+    if employee.save
+      render json: employee, status: :created
+    else
+      render json: { errors: employee.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # PUT /employees/:id
+  def update
+    if @employee.update(employee_params)
+      render json: @employee
+    else
+      render json: { errors: @employee.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /employees/:id
+  def destroy
+    @employee.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_employee
+    @employee = Employee.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Employee not found" }, status: :not_found
+  end
+
+  def employee_params
+    params.require(:employee).permit(
+      :first_name,
+      :last_name,
+      :job_title,
+      :department,
+      :country,
+      :salary,
+      :currency
+    )
+  end
+end
